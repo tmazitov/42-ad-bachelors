3b50f22fc84d95f135c780687585cc94002aa436
Timur Mazitov <timurmazitov000@gmail.com>
2026-05-25 17:40:35 +0400

    add ci/cd to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS && repo ? `/${repo}/` : '/',
   plugins: [tailwindcss(), vue()],
   resolve: {
     alias: {
